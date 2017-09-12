a2e2e4994247428d5fe3bd80a928ca7dfceeb62d
tomtrasmontero <tomtrasmontero@gmail.com>
2017-09-12 00:40:17 -0400

    modify user test spec to be more specific and detailed

--- test/server.test.js
+++ test/server.test.js
@@ -29,11 +29,17 @@ describe('routes', () => {
         })
         .then((result) => {
           expect(result.status).to.equal(200);
+          expect(result.body.name).to.equal('foo');
+          expect(result.body.id).to.equal(1);
+          expect(result.body.isAdmin).to.equal(true);
           return client(app).get('/api/user/1');
         })
         .then((result) => {
           expect(result.status).to.equal(200);
           expect(result.body.length).to.equal(1);
+          expect(result.body[0].name).to.equal('foo');
+          expect(result.body[0].id).to.equal(1);
+          expect(result.body[0].isAdmin).to.equal(true);
           done();
         });
     });
